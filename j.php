24d9aa6377e9c602749f7bb7809f4cf3e98ff5a8
arpitrohela <arpitrohela@gmail.com>
2017-04-16 18:30:16 +0530

    Add files via upload

---
+++ j.php
@@ -0,0 +1,82 @@
+<?php
+
+
+//API Details
+$username ="arpitrohela@gmail.com";
+$password ="password";
+$mob_number=$_POST['Mobile_no'];
+// Sender ID
+//Add a comment to this line
+$approved_senderid="SMSIND";
+
+//Approved Template
+$message = "Test sms from #field1#. Thanks for choosing our service - #field2# - SMSC Platform";
+$enc_msg= rawurlencode($message); // Encoded message
+
+//Create API URL
+$fullapiurl="http://smsc.biz/httpapi/send?username=$username&password=$password&sender_id=$approved_senderid&route=T&phonenumber=$mob_number&message=$enc_msg";
+
+//Call API
+$ch = curl_init($fullapiurl);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch); 
+//echo $result ; // For Report or Code Check
+curl_close($ch);
+echo "<p>SMS Request Sent - Message id - $result </p>";
+////////////////////////////////////////////////
+require 'PHPMailerAutoload.php';
+
+$mail = new PHPMailer;
+
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = '';                 // SMTP username
+$mail->Password = '';                           // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+
+$mail->setFrom('', '');
+//$mail->addAddress('', '');     // Add a recipient
+
+
+
+$email=$_POST['user_email'];
+$mail->addAddress($email,$email);     // Add a recipient
+//$mail->addAddress('', '');     // Add a recipient
+
+
+
+
+
+
+
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+
+
+/////////////////////////////////////////////////
+
+
+
+
+
+?>
